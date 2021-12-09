06bfd5e92555e919fe8b78bcf3e703667cb78734
Aaron <aaronkelly883@gmail.com>
2021-12-09 15:27:54 +0000

    added more working functions

--- server.js
+++ server.js
@@ -5,6 +5,7 @@ require('dotenv').config()
 var bodyParser = require("body-parser")
 const mongodb = require("mongodb")
 const mongoose = require("mongoose")
+const {ObjectId} = require("mongodb")
 
 mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -54,24 +55,107 @@ find.save();
 res.json({username:find.username+" - new user created", _id:find._id})
 
 console.log(find)
+delete find.__v
 }
 
 })
 
-app.post("/api/users/:_id/exercises", function(res, req){ 
-const {id} = req.param;
-
-res.json({"_id": id})
-
-
-})
 
 app.get("/api/users", (req, res)=>{ 
-  usersnames = req.query;
-  id = req.body._id;
-  lists = [usersnames, id];
-  res.json({users: lists})
+  
+
+  User.find({}, (err, arrayOfUsers)=>{
+    if(!err){ 
+      res.json(arrayOfUsers)
+    }
+  })
+  
 })
+
+app.param('_id', function(req, res, next) {
+	req.body._id = req.params._id;
+	next();
+});
+
+app.post("/api/users/:_id/exercises", function(res, req){ 
+const resObj = {};
+let id = req.body._id;
+
+if(id.length == 24){
+  let findOne = User.findOne({_id: ObjectId(id)});
+} else{ resObj["error"] = "no user with id:"+ id;
+return res.json(resObj);
+console.log(resObj);
+}
+
+let new_exercise = new Exercise({ 
+  description: req.body.description,
+  duration: parseInt(req.body.duration),
+  date: new Date(req.body.date)
+});
+
+if(!findOne){resObj["error"] = "no user with id"+ id
+res.json(resObj)
+console.log(resObj)} else{User.findByIdAndUpdate(id, 
+  {$push: {log: new_exercise}},
+  {new: true}, (err, updatedUser)=>{ 
+    resObj["_id"] = id
+    resObj["username"] =updatedUser.username
+    resObj["description"] = new_exercise.description
+    resObj["duration"] = new_exercise.duration
+    resObj["date"] = new Date(new_exercise.date).toDateString()
+    res.json(resObj)
+    console.log(resObj)
+  }  )}
+
+})
+
+app.get("/api/users/:_id/logs", (req, res)=>{ 
+const resObj = {};
+let id = req.body._id;
+
+if(id.length ==24){ 
+  var findOne =  User.findOne({_id: ObjectId(id)});
+}else{resObj["error"] ="no user with id: "+ id; 
+return res.json(resObj);
+console.log(resObj);}
+
+if(!findOne){ 
+resObj["erro"] = "no user with id: "+id;
+res.json(resObj);
+console.log(resObj);
+
+}else {
+  resObj["_id"] =id;
+  resObj["username"] = findOne.username;
+  resObj["count"] = findOne.log.length;
+  resObj["log"] = findone.log.map(obj=>({description: obj.description,
+  duration:obj.duration,
+date:obj.date.toDateString()}))
+
+
+if(req.query.from || req.query.to){ 
+  let fromDate = new Date(0).getTime();
+  let toDate = new Date().getTime();
+
+  if(req.query.from){fromDate = new Date(req.query.from).getTime();}
+  if(req.query.to){toDate =new Date(req.query.to).getTime();}
+
+  resObj.log = resObj.log.filter(exercise=>{let exerciseDate = new Date(exercise.date).getTime();
+  return exerciseDate >= fromDate && exerciseDate <= toDate;
+console.log(exerciseDate);});
+}
+
+if(req.query.limit){resObj.log.slice(0, req.query.limit);}
+resObj["count"]= resObj.log.length;
+
+res.json(resObj);
+console.log(resObj);}
+
+
+})
+
+
 
 
 
